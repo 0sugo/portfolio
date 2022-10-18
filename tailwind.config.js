4aea5174ce943188e7d391ef8665c755cbebce0c
osugo <josugo26@gmail.com>
2022-10-18 23:33:34 +0300

    Begin on landing page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        starter:['Creepster']
+      }
+    },
+  },
+  plugins: [],
+}
